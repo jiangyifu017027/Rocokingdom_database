0ced60b4256bee3205a370fea4ba6b2e133f4f94
jiangyifu017027 <2436537667@qq.com>
2023-06-15 20:31:19 +0800

    2023.6.15third

--- app.py
+++ app.py
@@ -3,30 +3,6 @@ import pymysql
 
 app = Flask(__name__)
 
-# 创建数据库连接
-conn = pymysql.connect(
-    host = '127.0.0.1', # 连接主机, 默认127.0.0.1 
-    user = 'root',      # 用户名
-    passwd = '123456',# 密码
-    port = 3306,        # 端口，默认为3306
-    db = 'lkwg',        # 数据库名称
-    charset = 'gb2312'    # 字符编码
-)
-
-# 生成游标对象 cursor
-cursor = conn.cursor()
-
-# 查询数据库版本
-cursor.execute("select version()") # 返回值是查询到的数据数量
-# 通过 fetchall方法获得数据
-data = cursor.fetchone()
-print("Database Version:%s" % data)
-
-cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
-
-cursor.close()  # 关闭游标
-conn.close()    # 关闭连接
-
 def conn_DB():
     global sql
     conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
@@ -38,7 +14,7 @@ def conn_DB():
     for i in data:
         dic={'用户名':i[0], '密码':i[1]}
         list_data.append(dic)
-
+    print(list_data)
     conn.commit()
     cursor.close()
     conn.close()
@@ -48,16 +24,29 @@ def conn_DB():
 def index():
     return render_template("index.html")
 
-@app.route("/login", methods=['POST','GET'])
-def login():
-    if request.method =='POST':
-        name = request.form.get('name')
-        passwd = request.form.get('passwd')
-        data = conn_DB()
-        for temp in data:
-            print(temp['用户名'], temp['密码'])
-            if temp['用户名'] == name and temp['密码']==passwd:
-                return redirect('/admin')
+@app.route("/login/<number>", methods=['POST','GET'])
+def login(number):
+    if number == '1':
+        if request.method =='POST':
+            name = request.form.get('name')
+            passwd = request.form.get('passwd')
+            data = conn_DB()
+            for temp in data:
+                print(temp['用户名'], temp['密码'])
+                if temp['用户名'] == name and temp['密码']==passwd:
+                    return redirect(url_for("index"))
+    if number == '0':
+        if request.method =='POST':
+            name = request.form.get('name')
+            passwd = request.form.get('passwd')
+            gamename = request.form.get('gamename')
+            conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+            cursor = conn.cursor()
+            sql = "INSERT INTO `player` (`id`, `gender`, `gameid`, `grade`, `family`, `password`) VALUES ('%s', 'man', '%s', 1200 , NULL,'%s');"%(name, passwd, gamename)
+            cursor.execute(sql)
+            conn.commit()
+            cursor.close()
+            conn.close()
     return render_template("Rocologin.html")
 
 
