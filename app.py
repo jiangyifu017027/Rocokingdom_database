4bd6c8a6e6626fcbaa978e4eb9d5c2ef26209a18
jiangyifu017027 <2436537667@qq.com>
2023-06-18 15:47:00 +0800

    2023.6.18.1

--- app.py
+++ app.py
@@ -24,31 +24,36 @@ def conn_DB():
 def index():
     return render_template("index.html")
 
-@app.route("/login/<number>", methods=['POST','GET'])
-def login(number):
-    if number == '1':
-        if request.method =='POST':
-            name = request.form.get('name')
-            passwd = request.form.get('passwd')
-            data = conn_DB()
-            for temp in data:
-                print(temp['用户名'], temp['密码'])
-                if temp['用户名'] == name and temp['密码']==passwd:
-                    return redirect(url_for("index"))
-    if number == '0':
-        if request.method =='POST':
-            name = request.form.get('name')
-            passwd = request.form.get('passwd')
-            gamename = request.form.get('gamename')
-            conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
-            cursor = conn.cursor()
-            sql = "INSERT INTO `player` (`id`, `gender`, `gameid`, `grade`, `family`, `password`) VALUES ('%s', 'man', '%s', 1200 , NULL,'%s');"%(name, passwd, gamename)
-            cursor.execute(sql)
-            conn.commit()
-            cursor.close()
-            conn.close()
-    return render_template("Rocologin.html")
+@app.route("/login", methods=['POST','GET'])
+def login():
+    if request.method =='POST':
+        name = request.form.get('name')
+        passwd = request.form.get('passwd')
+        data = conn_DB()
+        for temp in data:
+            print(temp['用户名'], temp['密码'])
+            if temp['用户名'] == name and temp['密码']==passwd:
+                return redirect(url_for("index"))
+    return render_template("Rocologin.html", show_login=True)
 
+@app.route("/register", methods=['POST','GET'])
+def register():
+    if request.method =='POST':
+        name = request.form.get('name')
+        passwd = request.form.get('passwd')
+        gamename = request.form.get('gamename')
+        conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+        cursor = conn.cursor()
+        sql = "INSERT INTO `player` (`id`, `gender`, `gameid`, `grade`, `family`, `password`) VALUES ('%s', 'man', '%s', 1200 , NULL,'%s');"%(name, passwd, gamename)
+        cursor.execute(sql)
+        conn.commit()
+        cursor.close()
+        conn.close()
+    return render_template("Rocologin.html", show_login=False)
+
+@app.route("/table")
+def table():
+    return render_template("table.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
