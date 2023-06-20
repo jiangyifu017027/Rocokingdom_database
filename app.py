8a0658db6faadb861cf7ef37ab039d6d3b814cdd
jiangyifu017027 <2436537667@qq.com>
2023-06-20 14:50:14 +0800

    2023.6.20洛克王国信息管理系统Ending

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 import pymysql
 from flask import session
 
@@ -118,12 +118,85 @@ def conn_DB6(inquiryid):
     conn.close()
     return list_data
 
+def conn_DB7(name):
+    global sql
+    conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+    cursor = conn.cursor()
+    sql = 'select exists (select `id` from `player` where `id` = "%s");'%(name)
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return data[0][0]
+
+def conn_DB8(deleteid,deletepetname):
+    global sql
+    conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+    cursor = conn.cursor()
+    sql = 'select exists (select `petid`,`petname` from `pet` where `petid`="%s" and `petname`="%s");'%(deleteid,deletepetname)
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return data[0][0]
+
+def conn_DB9(updateacti):
+    global sql
+    conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+    cursor = conn.cursor()
+    sql = 'select exists (select `actiid` from `activity` where `actiid`="%s");'%(updateacti)
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return data[0][0]
+
+def conn_DB10(uppetid):
+    global sql
+    conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+    cursor = conn.cursor()
+    sql = 'select exists (select `petid` from `pet` where `petid`="%s");'%(uppetid)
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return data[0][0]
+
+def conn_DB11(punishid):
+    global sql
+    conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+    cursor = conn.cursor()
+    sql = 'select exists (select `id` from `player` where `id`="%s");'%(punishid)
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return data[0][0]
+
+def conn_DB12(uppetid):
+    global sql
+    conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+    cursor = conn.cursor()
+    sql = 'select exists (select `petid` from `pet` where `petid`="%s");'%(uppetid)
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return data[0][0]
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/login", methods=['POST','GET'])
 def login():
+    loginbool = False
     if request.method =='POST':
         name = request.form.get('name')
         passwd = request.form.get('passwd')
@@ -138,6 +211,9 @@ def login():
             print(temp['用户名'], temp['密码'])
             if temp['用户名'] == name and temp['密码']==passwd:
                 return redirect(url_for("self"))
+                loginbool = True
+        if loginbool == False:
+            flash("Invalid id or password!", category="error")
     return render_template("Rocologin.html", show_login=True)
 
 @app.route("/register", methods=['POST','GET'])
@@ -145,15 +221,25 @@ def register():
     if request.method =='POST':
         name = request.form.get('name')
         passwd = request.form.get('passwd')
+        passwdnew = request.form.get('passwdnew')
         gamename = request.form.get('gamename')
-        conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
-        cursor = conn.cursor()
-        sql = "INSERT INTO `player` (`id`, `gender`, `gameid`, `grade`, `family`, `password`) VALUES ('%s', 'man', '%s', 1200 , NULL,'%s');"%(name, gamename, passwd)
-        cursor.execute(sql)
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return redirect(url_for("login"))
+        playerexist = conn_DB7(name)
+        print(playerexist)
+        print(type(playerexist))
+        if passwd != passwdnew:
+            flash("Confirm password error!", category="error")
+        else:
+            if playerexist:
+                flash("The account has been registered!", category="error")
+            else:
+                conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+                cursor = conn.cursor()
+                sql = "INSERT INTO `player` (`id`, `gender`, `gameid`, `grade`, `family`, `password`) VALUES ('%s', 'man', '%s', 1200 , 'ds','%s');"%(name, gamename, passwd)
+                cursor.execute(sql)
+                conn.commit()
+                cursor.close()
+                conn.close()
+            return redirect(url_for("login"))
     return render_template("Rocoregister.html", show_login=False)
 
 @app.route("/table")
@@ -199,16 +285,156 @@ def updateacti():
         upactiname = request.form.get('actiname')
         upreward = request.form.get('reward')
         upactitype = request.form.get('actitype')
+        data = conn_DB9(upactiid)
+        if data:
+            flash("This id has existed!", category="error")
+        else:
+            conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+            cursor = conn.cursor()
+            sql = "INSERT INTO `activity` (`actiid`, `actiname`, `reward`, `actitype`) VALUES ('%s', '%s', '%s','%s');"%(upactiid,upactiname,upreward,upactitype)
+            cursor.execute(sql)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return redirect(url_for("manager"))
+    return render_template("updateacti.html")
+
+@app.route("/updatepet", methods=['POST','GET'])
+def updatepet():
+    if request.method =='POST':
+        uppetid = request.form.get('petid')
+        updepartment = request.form.get('department')
+        upracevalue = request.form.get('racevalue')
+        upbloodline = request.form.get('bloodline')
+        upacqstate = request.form.get('acqstate')
+        upskill = request.form.get('skill')
+        uppetname = request.form.get('petname')
+        data = conn_DB10(uppetid)
+        if data:
+            flash("This pet has existed!", category="error")
+        else:
+            conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+            cursor = conn.cursor()
+            sql = "INSERT INTO `pet`(`petid`,`department`,`racevalue`,`bloodline`,`acqstate`,`skill`,`petname`) VALUES ('%s','%s','%d','%s','%s','%s','%s');"%(uppetid,updepartment,int(upracevalue),upbloodline,upacqstate,upskill,uppetname)
+            cursor.execute(sql)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return redirect(url_for("manager"))
+    return render_template("updatepet.html")
+
+@app.route("/petchange", methods=['POST','GET'])
+def petchang():
+    if request.method =='POST':
+        uppetid = request.form.get('petid')
+        updepartment = request.form.get('department')
+        upracevalue = request.form.get('racevalue')
+        upbloodline = request.form.get('bloodline')
+        upacqstate = request.form.get('acqstate')
+        upskill = request.form.get('skill')
+        uppetname = request.form.get('petname')
+        data = conn_DB11(uppetid)
+        if data == 0:
+            flash("This pet doesn't exist!", category="error")
+        else:
+            conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+            cursor = conn.cursor()
+            sql = "UPDATE `pet` SET (`petid`,`department`,`racevalue`,`bloodline`,`acqstate`,`skill`,`petname`) VALUES ('%s','%s','%d','%s','%s','%s','%s');"%(uppetid,updepartment,int(upracevalue),upbloodline,upacqstate,upskill,uppetname)
+            sql = "UPDATE `pet` SET `department`='%s' WHERE `petid`='%s'"%(updepartment,uppetid)
+            cursor.execute(sql)
+            conn.commit()
+            sql = "UPDATE `pet` SET `racevalue`='%d' WHERE `petid`='%s'"%(int(upracevalue),uppetid)
+            cursor.execute(sql)
+            conn.commit()
+            sql = "UPDATE `pet` SET `bloodline`='%s' WHERE `petid`='%s'"%(upbloodline,uppetid)
+            cursor.execute(sql)
+            conn.commit()
+            sql = "UPDATE `pet` SET `acqstate`='%s' WHERE `petid`='%s'"%(upacqstate,uppetid)
+            cursor.execute(sql)
+            conn.commit()
+            sql = "UPDATE `pet` SET `skill`='%s' WHERE `petid`='%s'"%(upskill,uppetid)
+            cursor.execute(sql)
+            conn.commit()
+            sql = "UPDATE `pet` SET `petname`='%s' WHERE `petid`='%s'"%(uppetname,uppetid)
+            cursor.execute(sql)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return redirect(url_for("manager"))
+    return render_template("petchange.html")
+
+@app.route("/punish", methods=['POST','GET'])
+def punish():
+    if request.method =='POST':
+        punishid = request.form.get('punishid')
+        sepunishid = request.form.get('sepunishid')
+        data = conn_DB12(punishid)
+        if data == 0:
+            flash("This player doesn't exist!", category="error")
+        else:
+            if punishid == sepunishid:
+                conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+                cursor = conn.cursor()
+                sql = 'DELETE FROM `player` WHERE `id` = "%s"'%(punishid)
+                cursor.execute(sql)
+                conn.commit()
+                cursor.close()
+                conn.close()
+                return redirect(url_for("manager"))
+    return render_template("punish.html")
+
+@app.route("/deletepet", methods=['POST','GET'])
+def deletepet():
+    if request.method =='POST':
+        deleteid = request.form.get('deleteid')
+        deletepetname = request.form.get('deletepetname')
+        playerid = session['id']
+        booldelete = conn_DB8(deleteid, deletepetname)
+        print(booldelete)
+        print(type(booldelete))
+        if booldelete:
+            conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+            cursor = conn.cursor()
+            sql = 'DELETE FROM `petbag` WHERE `id`="%s" and `petid`="%s"'%(playerid,deleteid)
+            cursor.execute(sql)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return redirect(url_for("self"))
+        else:
+            flash("放生宠物失败!", category="error")
+    return render_template("deletepet.html")
+
+@app.route("/passwdchange", methods=['POST','GET'])
+def passwdchange():
+    if request.method =='POST':
+        password = request.form.get('password')
+        passwordnew = request.form.get('passwordnew')
+        passwordnewnew = request.form.get('passwordnewnew')
+        playerid = session['id']
         conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
         cursor = conn.cursor()
-        sql = "INSERT INTO `activity` (`actiid`, `actiname`, `reward`, `actitype`) VALUES ('%s', '%s', '%s','%s');"%(upactiid,upactiname,upreward,upactitype)
+        sql = "SELECT `password` FROM `player` WHERE `id`='%s'"%(playerid)
         cursor.execute(sql)
+        data=cursor.fetchall()
         conn.commit()
         cursor.close()
         conn.close()
-        return redirect(url_for("manager"))
-    return render_template("updateacti.html")
-
+        if password != data:
+            flash("password error!", category="error")
+        else:
+            if passwordnew != passwordnewnew:
+                flash("Confirm password error!", category="error")
+            else:
+                conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", db="lkwg", charset="gb2312")
+                cursor = conn.cursor()
+                sql = "UPDATE `player` SET `password`='%s' WHERE `id`='%s'"%(passwordnew,playerid)
+                cursor.execute(sql)
+                conn.commit()
+                cursor.close()
+                conn.close()
+                return redirect(url_for("login"))
+    return render_template("passwdchange.html")
 
 
 if __name__ == '__main__':
